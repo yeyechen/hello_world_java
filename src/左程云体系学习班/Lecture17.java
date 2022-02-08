b703e25d8b6def86ef4fab9a4eda7782e9c48b22
yeye_razer <yeye.chen19@imperial.ac.uk>
2022-02-09 00:54:01 +0800

    lecture 17 notes and code update

--- src/左程云体系学习班/Lecture17.java
+++ src/左程云体系学习班/Lecture17.java
@@ -2,6 +2,7 @@ package 左程云体系学习班;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Stack;
 
 public class Lecture17 {
 
@@ -14,8 +15,11 @@ public class Lecture17 {
    * 1. 汉诺塔问题：n个圆盘，三个柱子分别为左，中，右。圆盘从小到大依次垒在左柱子上，怎么样把圆盘移动到右柱子上，并且
    * 同样从小到大。规定小圆盘上不能放大圆盘，并且只能移动柱子最顶端的圆盘。
    *
-   * 2. 打印字符串子序列问题：比如"abc" -> ["a", "b", "c", "ab", "ac", "bc, "abc"]
+   * 2. 字符串子序列问题：比如"abc" -> ["a", "b", "c", "ab", "ac", "bc", "abc"]。如果要求结果无重复，在用set。
    *
+   * 3. 字符串的全排列：比如"abc" -> ["abc", "acb", "bac", "bca", "cab", "cba"]。如果要求结果无重复，在交换前判断
+   *
+   * 4. 栈的逆序问题：给你一个栈, 请你逆序这个栈, 不能申请额外的数据结构, 只能使用递归函数。(其实就是用系统栈作为数据结构来实现)
    * */
 
   // 1. 汉诺塔问题
@@ -35,7 +39,7 @@ public class Lecture17 {
     }
   }
 
-  // 2. 打印字符串子序列问题
+  // 2. 字符串子序列问题
   public static List<String> subs(String s) {
     char[] str = s.toCharArray();
     String path = "";
@@ -58,8 +62,68 @@ public class Lecture17 {
     process1(str, index + 1, ans, path + str[index]);
   }
 
+  // 3. 字符串的全排列
+  public static List<String> permutation(String s) {
+    List<String> ans = new ArrayList<>();
+    if (s == null || s.length() == 0) {
+      return ans;
+    }
+    permProcess(s.toCharArray(), 0, ans);
+    return ans;
+  }
+
+  private static void permProcess(char[] s, int index, List<String> ans) {
+    if (index == s.length) {
+      ans.add(String.valueOf(s));
+      return;
+    }
+    boolean[] visited = new boolean[256]; // 去重, ascii数值范围是0-255
+    for (int i = index; i < s.length; i++) {
+      if (!visited[s[i]]) { // 去重, 如果之间交换过相同的字符, 那么跳过这次交换(剪枝)
+        visited[s[i]] = true;
+        // index位置的字符依次跟后面的位置的字符交换
+        swap(s, index, i);
+        permProcess(s, index + 1, ans);
+        // 恢复原始状态
+        swap(s, index, i);
+      }
+    }
+  }
+
+  private static void swap(char[] s, int i, int j) {
+    char temp = s[i];
+    s[i] = s[j];
+    s[j] = temp;
+  }
+
+  // 4. 栈的逆序问题
+  public static void reverse(Stack<Integer> stack) {
+    if (stack.empty()) {
+      return;
+    }
+    int last = lastElem(stack);
+    reverse(stack);
+    stack.push(last);
+  }
+
+  // 返回并移除栈底的元素, 比如 [1, 2, 3] 左边为栈顶, 右边为栈底, 变为 [1, 2], 并返回3
+  private static int lastElem(Stack<Integer> stack) {
+    int curr = stack.pop();
+    if (stack.empty()) {
+      return curr;
+    }
+    int last = lastElem(stack);
+    stack.push(curr);
+    return last;
+  }
+
   public static void main(String[] args) {
-    List<String> result = subs("abc");
-    System.out.println(result);
+    Stack<Integer> stack = new Stack<>();
+    stack.push(3);
+    stack.push(2);
+    stack.push(1);
+    System.out.println(stack);
+    reverse(stack);
+    System.out.println(stack);
   }
 }
