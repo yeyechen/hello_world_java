917cc4eccbe8b6df2625c20b3ae04a384fb90de3
yeyechen <yc1319@imperial.ac.uk>
2022-03-05 17:08:49 +0800

    lecture 23 NQueens problem; lecture 18 conclusion on Dynamic Programming

--- src/左程云体系学习班/Lecture23.java
+++ src/左程云体系学习班/Lecture23.java
@@ -13,6 +13,8 @@ public class Lecture23 {
    * 2. 数组分裂最小差值问题： 给定一个正数数组arr，请把arr中所有的数分成两个集合，尽量让两个集合的累加和接近，
    * 并且，如果数组长度为偶数，两个集合长度必须一样；如果数组长度为奇数，两个数组长度差值为1。
    * 返回：最接近的情况下，较小集合的累加和。
+   *
+   * 3. N皇后问题
    * */
 
   //1. 数组分裂最小差值问题
@@ -114,55 +116,80 @@ public class Lecture23 {
     }
   }
 
-  public static int right(int[] arr) {
-    if (arr == null || arr.length < 2) {
+  public static int NQueens(int n) {
+    if (n < 1) {
       return 0;
     }
-    int sum = 0;
-    for (int num : arr) {
-      sum += num;
-    }
-    if ((arr.length & 1) == 0) {
-      return process(arr, 0, arr.length / 2, sum / 2);
-    } else {
-      return Math.max(process(arr, 0, arr.length / 2, sum / 2), process(arr, 0, arr.length / 2 + 1, sum / 2));
-    }
+    int[] record = new int[n];
+    return process1(0, record, n);
   }
 
-  // arr[i....]自由选择，挑选的个数一定要是picks个，累加和<=rest, 离rest最近的返回
-  public static int process(int[] arr, int i, int picks, int rest) {
-    if (i == arr.length) {
-      return picks == 0 ? 0 : -1;
-    } else {
-      int p1 = process(arr, i + 1, picks, rest);
-      // 就是要使用arr[i]这个数
-      int p2 = -1;
-      int next = -1;
-      if (arr[i] <= rest) {
-        next = process(arr, i + 1, picks - 1, rest - arr[i]);
-      }
-      if (next != -1) {
-        p2 = arr[i] + next;
-      }
-      return Math.max(p1, p2);
+  // 当前来到i行，一共是0~N-1行
+  // 在i行上放皇后，所有列都尝试
+  // 必须要保证跟之前所有的皇后不打架
+  // int[] record record[x] = y 之前的第x行的皇后，放在了y列上
+  // 返回：不关心i以上发生了什么，i.... 后续有多少合法的方法数
+  public static int process1(int i, int[] record, int n) {
+    if (i == n) {
+      return 1;
     }
+    int res = 0;
+    for (int j = 0; j < n; j++) {
+      if (isValid(record, i, j)) {
+        record[i] = j;
+        res += process1(i + 1, record, n);
+      }
+    }
+    return res;
   }
 
+  public static boolean isValid(int[] record, int i, int j) {
+    // 0..i-1
+    for (int k = 0; k < i; k++) {
+      if (j == record[k] || Math.abs(record[k] - j) == Math.abs(i - k)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  // 用位运算来代表每个位置能否填入，大大加速运算速率
+  public static int NQueensOptimise(int n) {
+    if (n < 1 || n > 32) {
+      return 0;
+    }
+    // 如果你是13皇后问题，limit 最右13个1，其他都是0
+    int limit = n == 32 ? -1 : (1 << n) - 1;
+    return process2(limit, 0, 0, 0);
+  }
+
+  public static int process2(int limit, int colLim, int leftDiaLim, int rightDiaLim) {
+    if (colLim == limit) {
+      return 1;
+    }
+    // pos中所有是1的位置，是你可以去尝试皇后的位置
+    int pos = limit & (~(colLim | leftDiaLim | rightDiaLim));
+    int mostRightOne = 0;
+    int res = 0;
+    while (pos != 0) {
+      mostRightOne = pos & (~pos + 1);
+      pos = pos - mostRightOne;
+      res += process2(limit, colLim | mostRightOne, (leftDiaLim | mostRightOne) << 1,
+          (rightDiaLim | mostRightOne) >>> 1);
+    }
+    return res;
+  }
   public static void main(String[] args) {
-    int cycles = 10000;
-    System.out.println("测试开始");
-    for (int i = 0; i < cycles; i++) {
-      int[] arr = randomArray(10, 10);
-      int ans1 = minDiffSameSizeDp(arr);
-      int ans2 = right(arr);
-      if (ans1 != ans2) {
-        System.out.println(Arrays.toString(arr));
-        System.out.println(ans1);
-        System.out.println(ans2);
-        System.out.println("测试失败");
-        break;
-      }
-    }
-    System.out.println("测试结束");
+    int n = 10;
+
+    long start = System.currentTimeMillis();
+    System.out.println(NQueens(n));
+    long end = System.currentTimeMillis();
+    System.out.println("cost time: " + (end - start) + "ms");
+
+    start = System.currentTimeMillis();
+    System.out.println(NQueensOptimise(n));
+    end = System.currentTimeMillis();
+    System.out.println("cost time: " + (end - start) + "ms");
   }
 }
